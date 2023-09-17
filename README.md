3882bac6ade26ad77a8add608fcba336d096088e
Riju Mondal <pilotrijurocks@gmail.com>
2023-09-17 17:57:14 +0530

    first push

---
+++ README.md
@@ -0,0 +1,11 @@
+Hii everyone, I made this very simple parallax effect using **react-spring** library which uses physics instead of duration, timeframes in vanilla JS. 
+
+You can clone this repo and run this on your local machine by the following steps:
+
+1. Git clone **git@github.com:rijuoddusual1999/simpleparallax.git**
+
+2. Move to the project directory and download dependencies via **npm install**
+
+3. Host the project on your local machine, by running **npm run dev**
+
+Run all the above steps in the terminal of your favourite code editor. 
---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { startTransition, useState } from 'react'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import ('https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Fuggles&family=Handjet:wght@300&family=Indie+Flower&family=Lobster&family=Pacifico&family=Poppins:wght@200&family=Roboto+Condensed:wght@300&display=swap');
+import flatimage from './images/image1.jpg'
+import me from './images/me.webp'
+import war from './images/image2.jpg'
+
+function App() {
+  
+
+  return (
+    <div>
+      <Parallax pages={5} style={{top: '0', left: '0' ,position: 'absolute'}}>
+      <ParallaxLayer factor={4} offset={0}   style={{ textAlign: 'center', backgroundImage: `url(${flatimage})`,
+            backgroundSize: 'cover',}}>
+           
+      </ParallaxLayer>
+      <ParallaxLayer factor={1} offset={4}  style={{textAlign: 'center', backgroundImage:`url(${war})`,backgroundSize: 'cover'}}>
+         
+      </ParallaxLayer>
+
+
+      <ParallaxLayer sticky={{start: 0.9, end:4}} style= {{display:'flex', justifyContent: 'center'}}> 
+      <img src={me} />
+      </ParallaxLayer>
+
+      <ParallaxLayer
+          offset={0.5}
+          speed={0.5}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+           <h1 style={{textAlign: 'center'}}><p>Where do bad folks go when they die?</p> 
+ <p>They don't go to Heaven where the angels fly</p></h1>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={4.5}
+          speed={0.5}
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h1 style={{textAlign: 'center'}}><p>They go to the lake of fire and fry</p> 
+ <p>See 'em again 'til the fourth of July!</p></h1>
+        </ParallaxLayer>
+
+        
+
+      </Parallax>
+
+    </div>
+  )
+}
+
+export default App
